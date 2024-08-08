d1a73f66939ee7c90e86f878295d54c8d67a5e30
Nara <puniyoyo@gmail.com>
2024-08-08 11:03:21 +0800

    Initial Commit

---
+++ CPBLChatRoom/CPBLChatRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPBLChatRoomApp.swift
+//  CPBLChatRoom
+//
+//  Created by 林靖芳 on 2024/8/8.
+//
+
+import SwiftUI
+
+@main
+struct CPBLChatRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
