48d9341ed447472e24303cd3bb43b3b072b2405b
Nara <puniyoyo@gmail.com>
2024-08-30 00:31:32 +0800

    Refactor: Implement tab management using enum
    - Create Tab enum to represent different tabs
    - Use CaseIterable for automatic tab generation
    - Improve code maintainability and type safety
    - Simplify TabView implementation with ForEach and allCases

--- CPBLChatRoom/View/MainView.swift
+++ CPBLChatRoom/View/MainView.swift
@@ -7,41 +7,52 @@
 
 import SwiftUI
 
+extension MainView {
+    enum Tab: View, CaseIterable {
+        case chat, profile
+        
+        var body: some View {
+            content.tabItem{ tabLabel }
+        }
+        
+        @ViewBuilder
+        private var content: some View {
+            switch self {
+                case .chat:
+                    ChatView()
+                case .profile:
+                    ProfileView()
+            }
+        }
+        
+        private var tabLabel: some View {
+            switch self {
+                case .chat:
+                    Label("Chat", systemImage: "text.bubble")
+                case .profile:
+                    Label("Profile", systemImage: "person.circle")
+            }
+        }
+        
+    }
+}
+
+
 struct MainView: View {
     @StateObject var viewModel = MainViewViewModel()
         
     var body: some View {
         if viewModel.isSignIn, !viewModel.currentUserId.isEmpty {
             //已登入，到主頁
-            accountView
+            TabView {
+                ForEach(Tab.allCases, id: \.self) { $0 }
+            }
             .tint(Color.main)
-            
-          
         } else{
             //未登入，到登入頁面
            LoginView()
-            
-            
-                
         }
     }
-    
-    @ViewBuilder
-    var accountView: some View {
-        TabView {
-            ChatView(viewModel: ChatViewViewModel())
-                .tabItem {
-                    Label("Chat", systemImage: "text.bubble")
-                }
-            ProfileView()
-                .tabItem {
-                    Label("Profile", systemImage: "person.circle")
-                }
-
-        }
-    }
-    
-    
 }
 
 //#Preview {
